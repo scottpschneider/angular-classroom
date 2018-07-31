9e83f48053727c4553e0b2c4c2666ec8bd0a0ff9
Scott <schneider_scott@live.com>
2018-07-31 06:22:18 -0600

    students component, show, selector element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './students/students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule
